a773cc299c96d6df9dd48a6608351c0bc97bd5bd
R.SIRHC <christopher.r99@live.com>
2016-05-31 12:50:36 -0400

    Add project files.

---
+++ COMP123-Lesson4/Person.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Lesson4
+{
+    /**
+     * This class defines a generic Person
+     * 
+     * @class Perosn
+     * @field {string} _name 
+     * @field {int} _age
+     */
+    public class Person
+    {
+        //Private instance variables/fields ++++++++++++++++++++++++++++++++++++++++++++++
+        private string _name;
+        private int _age;
+
+
+        //Public properties ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+        /**
+         * 
+         * <summary> 
+         * This is a public property for our _name field
+         * </summary>
+         * 
+         * @property {string} Name
+         */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        }
+
+
+        /**
+         * <summary>
+         * This is a public property for our _age field
+         * </summary>
+         * 
+         * @property {int} age
+         */
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                this._age = value;
+            }
+        }
+
+        /**
+         * <summary>
+         * This is the default empty constructor for the Person class
+         * </summary>
+         * 
+         * @constructor Person
+         */
+        public Person()
+        {
+            this._initialize();
+        }
+
+
+        /**
+         * <summary>
+         * This is a constructor that takes name as a parameter and 
+         * passes t to the _name private instance variable
+         * </summary>
+         * 
+         * @constructor Person
+         * @param {string} 
+         */
+        public Person(string name)
+        {
+            this._initialize();
+            this.Name = name;
+        }
+
+        /**
+         * <summary>
+         * This is a constructor that takes age as a parameter and 
+         * passes it to the _age private instance variable
+         * </summary>
+         * 
+         * @constructor Person
+         * @param {int} age
+         * 
+         */
+
+        public Person(int age)
+        {
+            this._initialize();
+            this.Age = age;
+        }
+
+
+        /**
+         * <summary>
+         * This is a constructor that takes name and age as parameters.  
+         * It assigns the local name value to the name property. 
+         * It assigns the local age value to the age property. 
+         * </summary>
+         * 
+         * @constructor Person
+         * @param {string} name
+         * @param {int} age
+         * 
+         */
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        //Private method ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+        /**
+         * <summary>
+         * This method initializes the public properties Name and Age
+         * </summary>
+         * 
+         * @private
+         * @method _initialize
+         * @returns {void}
+         * 
+         * 
+         */
+        private void _initialize()
+        {
+            // Initialize public properties with some values
+            this.Name = "unknown name!";
+            this.Age = 0;
+        }
+
+
+        //Public method ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This method outputs the _name value with " says Hello!!" to the console. 
+         * </summary>
+         * 
+         * @method _SaysHello
+         * @return {void}
+         * 
+         */
+        public void SaysHello()
+        {
+            Console.WriteLine(this.Name + " says Hello!!");
+        }
+
+
+        /**
+         * <summary>
+         * This method outputs the _name and _age value in the following
+         * format: _name + " is " + _age + " years old."
+         * </summary>
+         * 
+         * @method ShowAge
+         * @return {void}
+         */
+
+        public void ShowAge()
+        {
+            Console.WriteLine(this.Name + " is " + this.Age + " years old.");
+        }
+    }
+}
---
+++ COMP123-Lesson4/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Author: Christopher Ritchil 
+ * Date created: May 27 2016
+ * Date Modified: May 27 2016
+ * Student ID: 300702644
+ * 
+ * Description: This program demonstrate creating Person class in seperate file, private instances for the class, public property, 
+ *              show age & say hello methods for the Person class, and finally Person object in the driver class. 
+ * 
+ * Version: 0.1.0 
+ */
+
+
+
+namespace COMP123_Lesson4
+{
+
+    /**
+     * This class is the driver class for our Program
+     */
+
+
+    public class Program
+    {
+        /**
+         *the main method for our driver class Program
+         * 
+         * 
+         * @method Main 
+         * @param {string} args 
+         */
+        public static void Main(string[] args)
+        {
+            //create a new instance of the Person class
+            Person Sarah = new Person();
+            Sarah.Name = "Sarah";
+            Sarah.Age = 29;
+            Sarah.SaysHello();
+            Sarah.ShowAge();
+
+            Console.WriteLine("-----------------------------");
+
+            Person Chris = new Person("Tom");
+            Chris.Age = 23;
+            Chris.SaysHello();
+            Chris.ShowAge();
+
+            Console.WriteLine("-----------------------------");
+
+            Person mark = new Person(30);
+            mark.Name = "Mark";
+            mark.SaysHello();
+            mark.ShowAge();
+
+            Console.WriteLine("-----------------------------");
+
+            Person Joanne = new Person("Joanne", 25);
+            Joanne.SaysHello();
+            Joanne.ShowAge();
+
+
+            Console.WriteLine("-----------------------------");
+
+            Person Jing = new Person("Jing", 35);
+            Jing.SaysHello();
+            Jing.ShowAge();
+
+
+            Console.WriteLine("-----------------------------");
+
+            Person Kent = new Person("Kent", 29);
+            Kent.SaysHello();
+            Kent.ShowAge();
+
+        }
+    }
+}
